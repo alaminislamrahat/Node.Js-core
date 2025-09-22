9a286eca9734c6e842a42875a5e15e4b6963d0d3
alaminislamrahat <alaminislamrahat@gmail.com>
2025-09-22 23:07:19 +0600

    server created

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ const fs = require("fs");
 
 
 const myServer = http.createServer((req, res) => {
-    const log = `${Date.now()}:New Request Recieved\n`;
+    const log = `${Date.now()}:${req.url}:New Request Recieved\n`;
     fs.appendFile('log.txt', log, (err, data) => {
-        res.end("hello from server")
+
+        switch (req.url) {
+            case '/': res.end("home page")
+                break;
+            case '/about': res.end("about al amin islam rahat");
+                break;
+            default: res.end("404")
+
+        }
 
     })
 });
