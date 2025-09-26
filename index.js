36dd1107811fc7e917c0a4cd2bdff7249f132373
alaminislamrahat <alaminislamrahat@gmail.com>
2025-09-26 11:38:09 +0600

    now I will strart express

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const http = require("http");
 const fs = require("fs");
+const url = require('url')
 
 
 const myServer = http.createServer((req, res) => {
     const log = `${Date.now()}:${req.url}:New Request Recieved\n`;
+
+    const myUrl = url.parse(req.url, true)
+    console.log(myUrl)
     fs.appendFile('log.txt', log, (err, data) => {
 
-        switch (req.url) {
+        switch (myUrl.pathname) {
             case '/': res.end("home page")
                 break;
-            case '/about': res.end("about al amin islam rahat");
+            case '/about': 
+            const username = myUrl.query.name;
+            res.end(`hi ${username}`);
                 break;
             default: res.end("404")
 
